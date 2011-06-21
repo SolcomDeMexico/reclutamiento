6fd69611a12df4e8526a37480ea192eea504e408
andresmb <andres.muro@solcomla.com>
2011-06-21 14:08:18 -0500

    NombreEntrevista

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-require './.bundle/gems/rails'
+#require 'rails'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
