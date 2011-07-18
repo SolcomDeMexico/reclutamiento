1be535a98e84b784a359905914d566a2d093bf61
unknown <andres.muro@.(none)>
2011-07-18 10:25:24 -0500

    Prueba AcessToken

--- app/controllers/sesion_controller.rb
+++ app/controllers/sesion_controller.rb
@@ -13,24 +13,25 @@ require 'Users'
     #access_token = client.web_server.get_access_token(params[:code], :redirect_uri => RECLUTAMIENTO_PATH, :grant_type => 'authorization_code')
 		ENV['sfdc_token'] = request.env['omniauth.auth']['credentials']['token']
     ENV['sfdc_instance_url'] = request.env['omniauth.auth']['instance_url']    
-    id = request.env['omniauth.strategy'].inspect[1061,69]
-    user = Users.get_uri(id)    
-    user_id = user['user_id']    
-		usuario = Usuario.find_by_user_id(user_id)
-    if usuario.nil?
-		  email = user['email']
-		  picture = user['photos']['picture']
-		  nombre = user['display_name'] 
-      horario = user['utcOffset']
-		  usuario = Usuario.new(:user_id => user_id, :email => email, :picture => picture, :display_name => nombre, 
-                         :zona_horaria => horario, :role => "empleado") 
-		  usuario.save
-      usuario.updated_by = usuario.id
-      usuario.save
-		  usuario = Usuario.find_by_user_id(user_id)
-    end 
-    sign_in usuario    
-		redirect_to  :action => 'home'
+    render :text => request.env.inspect
+    #id = request.env['omniauth.strategy'].inspect[1061,69]
+    #user = Users.get_uri(id)    
+    #user_id = user['user_id']    
+		#usuario = Usuario.find_by_user_id(user_id)
+    #if usuario.nil?
+		#  email = user['email']
+		#  picture = user['photos']['picture']
+		#  nombre = user['display_name'] 
+    #  horario = user['utcOffset']
+		#  usuario = Usuario.new(:user_id => user_id, :email => email, :picture => picture, :display_name => nombre, 
+    #                     :zona_horaria => horario, :role => "empleado") 
+		#  usuario.save
+    #  usuario.updated_by = usuario.id
+    #  usuario.save
+		#  usuario = Usuario.find_by_user_id(user_id)
+    #end 
+    #sign_in usuario    
+		#redirect_to  :action => 'home'
   end
 
   def destroy 
